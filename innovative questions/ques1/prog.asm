b5c7b6aab1d0a21a4a23398ea6035ec1e6dede5e
Amanskywalker <ak47su30ac@gmail.com>
2016-04-11 17:17:13 +0530

    initial codes added  	modified:   innovative questions/ques1/prog.asm

--- innovative questions/ques1/prog.asm
+++ innovative questions/ques1/prog.asm
@@ -3,7 +3,8 @@
 .model small
 .data
   ipmsg db 10,13,"Enter your the review about the product :$"
-  uip db 100h dup(?)  ;user input is stored in it
+  uip db 100h dup(0)  ;user input is stored in it
+  tstr db 100h dup(0)   ;temp string to hold the words for comparison
 
   ; defining the words for positive review
   g1 db "awesome"
@@ -19,11 +20,21 @@
   bc db 00h
 
   ;size of user input
+  n db 00h
+
+  ; including the macros
+  include read.mac
+  include write.mac
+  include strcmp.mac     ;macro for comparing the stings
+
 
 .code
   mov ax,@data
   mov ds,ax
+  mov es,ax       ; required for string comparison
 
   read uip n      ;macro to read from user
 
-  
+  ; process to read from user input string using space as delimiter
+  ;and using strcmp to find wether give word matches or not and increment the appropriate counter
+  ;at last printing the result    
